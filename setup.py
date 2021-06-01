e4fe9dea6aa2d7b84f48e18ed807f441271aa76a
Jean-Christophe Fabre <jean-christophe.fabre@inrae.fr>
2021-06-01 21:54:37 +0200

    Cleaning of code

--- setup.py
+++ setup.py
@@ -6,29 +6,30 @@ __author__ = "Jean-Christophe Fabre <jean-christophe.fabre@inrae.fr>"
 
 from setuptools import setup
 
+
 def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name = 'wecoded',
-      version = '0.1',
-      description = 'A tool for counting contributed lines by authors on multiple git repositories',
-      long_description = readme(),
-      author = 'Jean-Christophe Fabre',
-      author_email = 'jean-christophe.fabre@inrae.fr',
-      url = 'https://github.com/jctophefabre/wecoded',
-      license = 'MIT',
-      packages = ['wecoded'],
-      package_data = {
+
+setup(name='wecoded',
+      version='0.1',
+      description='A tool for counting contributed lines by authors on multiple git repositories',
+      long_description=readme(),
+      author='Jean-Christophe Fabre',
+      author_email='jean-christophe.fabre@inrae.fr',
+      url='https://github.com/jctophefabre/wecoded',
+      license='MIT',
+      packages=['wecoded'],
+      package_data={
           'rejocker': ['resources/*']
       },
-      entry_points = {
+      entry_points={
           'console_scripts': [
-              'wecoded = wecoded.__main__:main'
+              'wecoded=wecoded.__main__:main'
           ]
       },
-      include_package_data = True,
-      install_requires = [
+      include_package_data=True,
+      install_requires=[
       ],
-      python_requires = '~=3.6',
-     )
+      python_requires='~=3.6')
--- wecoded/__main__.py
+++ wecoded/__main__.py
@@ -9,86 +9,72 @@ import csv
 
 class WeCoded():
 
-    def __init__(self,args):
+    def __init__(self, args):
         self.args = args
         self.config = None
         self.all_stats = dict()
         self.found_authors = set()
 
-
     def __load_config(self):
         with open(self.args["config_file"], 'r') as YAMLFile:
-          self.config = yaml.load(YAMLFile, Loader=yaml.FullLoader)
+            self.config = yaml.load(YAMLFile, Loader=yaml.FullLoader)
 
-
-    def get_author_name(self,author):
+    def get_author_name(self, author):
         for n in self.config["authors"]:
             if author in n["aliases"]:
                 return n["name"]
         return author
 
-
     def run(self):
-
         os.makedirs(self.args["workpath"], exist_ok=True)
-
         self.__load_config()
-
         if not self.args["no_clone"]:
             self.clone_repositories()
-
         if not self.args["no_stats"]:
             self.compute_stats()
             self.write_stats()
 
-
     def clone_repositories(self):
-        
         for r in self.config["repositories"]:
-            print("### Cloning",r["name"],"####################")
-            cmd = ["git","clone",r["url"],"-b",r["revision"], r["name"]]
+            print("### Cloning", r["name"], "####################")
+            cmd = ["git", "clone", r["url"], "-b", r["revision"], r["name"]]
             subprocess.run(cmd, cwd=self.args["workpath"])
-
 
     def compute_stats(self):
         for r in self.config["repositories"]:
-            print("### Computing stats for",r["name"],"####################")
-            cmd = ["git","summary","--line"]
-            ret = subprocess.run(cmd, cwd=os.path.join(self.args["workpath"],r["name"]),capture_output=True)
+            print("### Computing stats for", r["name"], "####################")
+            cmd = ["git", "summary", "--line"]
+            ret = subprocess.run(cmd, cwd=os.path.join(self.args["workpath"], r["name"]), capture_output=True)
             output = ret.stdout.decode("utf-8")
             lines = output.split("\n")
-            stats = dict();
+            stats = dict()
             lines = [x for x in lines if x]  # remove empty lines
-            for l in lines :
-                if ':' not in l:
-                    data = [x for x in l.split(' ') if x]
+            for line in lines:
+                if ':' not in line:
+                    data = [x for x in line.split(' ') if x]
                     data = data[:-1]  # remove latest field (percentage)
                     count = int(data[0])
-                    author = " ".join(data[1:]) # join all fields fbut first for author name
+                    author = " ".join(data[1:])  # join all fields fbut first for author name
                     author = self.get_author_name(author)
                     if author not in stats:
                         stats[author] = 0
                     stats[author] += count
                     self.found_authors.add(author)
-
             self.all_stats[r["name"]] = stats
 
-
     def write_stats(self):
-
-        with open(os.path.join(self.args["workpath"],"stats.csv"),'w',newline='') as csvfile:
+        with open(os.path.join(self.args["workpath"], "stats.csv"), 'w', newline='') as csvfile:
             fields = list()
             fields.append('author')
-            default_row = { 'author' : ''}
+            default_row = {'author': ''}
             for r in self.config["repositories"]:
                 fields.append(r["name"])
-                default_row[r["name"]] = 0;
+                default_row[r["name"]] = 0
             writer = csv.DictWriter(csvfile, fieldnames=fields, delimiter=';')
             writer.writeheader()
-            
             for a in self.found_authors:
                 row = default_row.copy()
-                row['author'] = a                
+                row['author'] = a
                 for r in self.config["repositories"]:
                     repos = r["name"]
                     if a in self.all_stats[repos].keys():
@@ -97,10 +83,11 @@ class WeCoded():
 
 
 def main():
-    parser = argparse.ArgumentParser(description="A tool for contributions stats by authors on multiple git repositories")
+    parser = argparse.ArgumentParser(description=("A tool for contributions stats by authors "
+                                                  "on multiple git repositories"))
     parser.add_argument("workpath", type=str,
                         help="the work path")
-    parser.add_argument("-f", "--config-file", type=str, default=os.path.join(os.getcwd(),"wecoded-config.yaml"),
+    parser.add_argument("-f", "--config-file", type=str, default=os.path.join(os.getcwd(), "wecoded-config.yaml"),
                         help='the path to the YAML configuration file (default: %(default)s)')
     parser.add_argument("-c", "--no-clone", action="store_true", default=False,
                         help="disable the cloning of remote repositories")
